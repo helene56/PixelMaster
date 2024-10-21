7fe51bc83e2372c2f42a5d6e4e7cecf650d05111
helene <helene.maja@hotmail.dk>
2024-10-21 20:42:02 +0200

    first iteration of program

--- PixelMaster.cpp
+++ PixelMaster.cpp
@@ -1,12 +1,30 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
+
 
 constexpr int PIN16 {16};
+
+// function prototypes
+
+inline void set_high(int pin);
+inline void set_low(int pin);
+// transfer 0 high
+void T0H();
+// transfer 1 high
+void T1H();
+// transfer 0 low
+void T0L();
+// transfer 1 low
+void T1L();
+
 
 int main()
 {
     stdio_init_all();
-
+    gpio_init(PIN16);
+    gpio_set_dir(PIN16, GPIO_OUT);
+    
     while (true) {
         printf("Hello, world!\n");
         sleep_ms(1000);
@@ -15,5 +33,52 @@ int main()
 
 void T0H()
 {
-    
+    set_high(PIN16);
+    busy_wait_us(0.4);
+}
+
+void T1H()
+{
+    set_high(PIN16);
+    busy_wait_us(0.8);
+}
+
+void T0L()
+{
+    set_low(PIN16);
+    busy_wait_us(0.85);
+
+}
+
+void T1L()
+{
+    set_low(PIN16);
+    busy_wait_us(0.45);
+}
+
+void code0()
+{
+    T0H();
+    T0L();
+}
+
+void code1()
+{
+    T1H();
+    T1L();
+}
+
+void reset_code()
+{
+    // above 50us to reset
+    busy_wait_us(100);
+}
+
+inline void set_high(volatile int pin)
+{
+    sio_hw->gpio_set = (1 << pin);
+}
+inline void set_low(volatile int pin)
+{
+    sio_hw->gpio_clr = (1 << pin);
 }
