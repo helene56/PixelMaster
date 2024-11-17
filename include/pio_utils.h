a78d8038f6c7c7a09f5691cbff91ccc2a03ec8ba
helene <helene.maja@hotmail.dk>
2024-11-17 23:38:42 +0100

    update sleep time to correctly reset led lights

--- include/pio_utils.h
+++ include/pio_utils.h
@@ -13,4 +13,9 @@ inline uint32_t ugrb_u32(uint8_t g, uint8_t r, uint8_t b)
     return ((uint32_t) (g) << 16) | ((uint32_t) (r) << 8) |(uint32_t) (b);
 }
 
+inline void reset_pixel()
+{
+    sleep_us(400);
+}
+
 #endif // PIO_UTILS
