20c58c2be9ff0d0403598b208a98e2dbc3aaf244
helene <helene.maja@hotmail.dk>
2024-11-25 23:21:54 +0100

    clean slate for new program

--- src/main.cpp
+++ src/main.cpp
@@ -39,15 +39,6 @@ int main()
     gpio_set_dir(Pins::button, GPIO_IN);
     gpio_pull_up(Pins::button);
 
-    // initialize read light
-    gpio_init(Pins::read_light);
-    gpio_set_dir(Pins::read_light, GPIO_IN);
-    gpio_pull_down(Pins::read_light);
-
-    // initialize light
-    gpio_init(Pins::light);
-    gpio_set_dir(Pins::light, GPIO_OUT);
-    gpio_put(Pins::light, 1);
     PIO pio = pio0;
     int sm = 0;
     uint offset = pio_add_program(pio, &wave_program);
@@ -57,129 +48,18 @@ int main()
 
     std::uint32_t w {0x050505};
     std::uint32_t y {0x030301}; // yellow at 5% brightness
-    std::uint32_t neutral[8][8] {0, 0, 0, 0, 0, 0, 0, 0,
-                               0, 0, 0, 0, 0, 0, 0, 0,
-                               0, 0, 0, 0, 0, 0, 0, 0,
-                               0, w, w, 0, 0, w, w, 0,
-                               0, 0, 0, 0, 0, 0, 0, 0,
-                               0, 0, 0, w, w, 0, 0, 0,
-                               0, 0, 0, 0, 0, 0, 0, 0,
-                               0, 0, 0, 0, 0, 0, 0, 0,};
-
-    std::uint32_t face2[8][8] {0, 0, 0, 0, 0, 0, 0, 0,
-                               0, 0, 0, 0, 0, 0, 0, 0,
-                               0, w, 0, 0, 0, 0, w, 0,
-                               0, 0, w, 0, 0, w, 0, 0,
-                               0, w, 0, 0, 0, 0, w, 0,
-                               0, 0, 0, w, w, 0, 0, 0,
-                               0, 0, 0, 0, 0, 0, 0, 0,
-                               0, 0, 0, 0, 0, 0, 0, 0,};
-
-    std::uint32_t face3[8][8] {0, 0, 0, 0, 0, 0, 0, 0,
-                               0, 0, 0, 0, 0, 0, 0, 0,
-                               0, 0, 0, 0, 0, 0, 0, 0,
-                               w, w, 0, 0, w, w, 0, 0,
-                               0, 0, 0, 0, 0, 0, 0, 0,
-                               0, 0, 0, w, w, 0, 0, 0,
-                               0, 0, 0, 0, 0, 0, 0, 0,
-                               0, 0, 0, 0, 0, 0, 0, 0,};
-
-    std::uint32_t face4[8][8] {0, 0, 0, 0, 0, 0, 0, 0,
-                               0, 0, 0, 0, 0, 0, 0, 0,
-                               0, 0, 0, 0, 0, 0, 0, 0,
-                               0, 0, w, w, 0, 0, w, w,
-                               0, 0, 0, 0, 0, 0, 0, 0,
-                               0, 0, 0, w, w, 0, 0, 0,
-                               0, 0, w, 0, 0, w, 0, 0,
-                               0, 0, 0, w, w, 0, 0, 0,};
-
-    std::uint32_t happy[8][8] {0, 0, 0, 0, 0, 0, 0, 0,
-                               0, 0, 0, 0, 0, 0, 0, 0,
-                               0, w, 0, 0, 0, 0, w, 0,
-                               w, 0, w, 0, 0, w, 0, w,
-                               0, 0, 0, 0, 0, 0, 0, 0,
-                               0, 0, 0, w, w, 0, 0, 0,
-                               0, 0, 0, 0, 0, 0, 0, 0,
-                               0, 0, 0, 0, 0, 0, 0, 0,};
-
-    std::uint32_t light_bulb0[8][8] {0, 0, 0, 0, 0, 0, 0, 0,
-                                     0, 0, 0, 0, 0, 0, 0, 0,
-                                     0, 0, 0, w, w, 0, 0, 0,
-                                     0, 0, w, 0, 0, w, 0, 0,
-                                     0, 0, w, 0, 0, w, 0, 0,
-                                     0, 0, 0, w, w, 0, 0, 0,
-                                     0, 0, 0, w, w, 0, 0, 0,
-                                     0, 0, 0, 0, 0, 0, 0, 0,};
     
-    std::uint32_t light_bulb1[8][8] {0, 0, 0, 0, 0, 0, 0, 0,
-                                     0, 0, 0, y, y, 0, 0, 0,
-                                     0, 0, y, w, w, y, 0, 0,
-                                     0, y, w, 0, 0, w, y, 0,
-                                     0, 0, w, 0, 0, w, 0, 0,
-                                     0, y, 0, w, w, 0, y, 0,
-                                     0, 0, y, w, w, y, 0, 0,
-                                     0, 0, 0, 0, 0, 0, 0, 0,};
-    
-    std::uint32_t light_bulb2[8][8] {0, 0, y, 0, 0, y, 0, 0,
-                                     0, y, 0, y, y, 0, y, 0,
-                                     0, 0, y, w, w, y, 0, 0,
-                                     y, y, w, 0, 0, w, y, y,
-                                     0, 0, w, 0, 0, w, 0, 0,
-                                     0, y, 0, w, w, 0, y, 0,
-                                     y, 0, y, w, w, y, 0, y,
-                                     0, y, 0, 0, 0, 0, y, 0,};
-    
-    std::uint32_t (*annoyed[2])[8] = {neutral, face2};
-    std::uint32_t (*happyface[2])[8] = {neutral, happy};
+
+
     while (true) 
     {
 
         uint32_t current_time = to_ms_since_boot(get_absolute_time());
-        if (!gpio_get(Pins::button))
-        { 
-            frames::button_pressed = true;
-        }
-
-        if (frames::button_pressed)
-        {
-            for (int i = 0; i < 2; ++i)
-            {
-                face(neutral);
-                sleep_ms(200);
-                face(face2);
-                sleep_ms(200);
-            }
-            sleep_ms(200);
-            frames::button_pressed = false;
-        }
-
-        if (gpio_get(Pins::read_light))
-        {
-            face(light_bulb0);
-            while(gpio_get(Pins::read_light))
-            {
-                sleep_ms(400);
-                face(light_bulb1);
-                sleep_ms(400);
-                face(light_bulb2);
-                sleep_ms(400);
-                face(light_bulb0);
-            }
-            sleep_ms(2);
-        }
+        
         // Check if it's time to switch frames
         if (current_time - frames::last_frame_time >= frames::FRAME_INTERVAL) 
         {
-            if (frames::is_face_1) 
-            {
-                face(happy);
-            } 
-            else 
-            {
-                face(neutral);
-            }
-            frames::is_face_1 = !frames::is_face_1; // Toggle face
-            frames::last_frame_time = current_time;
+            
         }
         // small delay for button to be read correctly
         sleep_ms(2);
